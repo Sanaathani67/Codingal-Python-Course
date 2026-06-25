e794a22a68aa203c9ed454753429e2f6672394dd
Sanaathani Sithara <dr.jyothini.mallesh@gmail.com>
2026-06-25 12:52:33 +0530

    finished l2 a2

---
+++ Module 1 basics/Lessson 2/activity2.py
@@ -0,0 +1,19 @@
+name="sanaathani"
+print(name)
+print("my name is ",name)
+
+my_fav_player="ronaldo"
+print("my_fav_player",my_fav_player)
+
+friend1="Maryam"
+#1friend cannot be a variable
+print(friend1)
+
+
+import keyword
+
+# print all the Python keywords
+
+print("Python keywords are...\n")
+
+print(keyword.kwlist)
