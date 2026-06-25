7e5f2cd3ccda24774b323bed95f7965efedcb0d5
Sanaathani Sithara <dr.jyothini.mallesh@gmail.com>
2026-06-25 12:30:48 +0530

    finished l2 a1

---
+++ Module 1 basics/Lessson 2/activity1.py
@@ -0,0 +1,13 @@
+print("welcome to the world of programming!")
+print("67")
+print(67)
+
+
+print("hello \n\n\n\n\n\n\n\n\n\n\n")
+print("python")
+
+print("shrek","fiona")
+print("5+4=",5+4)
+
+print("Christiano",end="\n")
+print("Ronaldo")
