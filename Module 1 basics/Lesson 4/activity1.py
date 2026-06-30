8fbf374213afadb287b144e1b30dcd39081ca70e
Sanaathani Sithara <dr.jyothini.mallesh@gmail.com>
2026-06-30 11:58:36 +0530

    finished l4 m1 a1

---
+++ Module 1 basics/Lesson 4/activity1.py
@@ -0,0 +1,17 @@
+# M1 L4 A1
+
+# 1) Store the values for each tree in separate variables: tree1, tree2, tree3, tree4, tree5
+tree1=97
+tree2=67
+tree3=69
+tree4=104
+tree5=50
+# 2) Add all 5 tree values and store the result in `sum`
+sum=tree1+tree2+tree3+tree4+tree5
+
+# 3) Print the total (`sum`) of all 5 trees
+print("the sum of all tree heights =",sum)
+# 4) Divide `sum` by 5 to find the average and store it in `average`
+average=sum/5
+# 5) Print the average (`average`) of all 5 trees
+print("avg height =",average)
