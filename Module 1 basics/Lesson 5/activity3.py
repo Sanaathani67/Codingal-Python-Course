c45b7e7af2052f9ee7335f87311d734108bda1f5
Sanaathani Sithara <dr.jyothini.mallesh@gmail.com>
2026-07-01 12:21:47 +0530

    finished m1 l5 a3

---
+++ Module 1 basics/Lesson 5/activity3.py
@@ -0,0 +1,8 @@
+ronaldo_scores_a_goal=input("did he score today?yes or no =")
+if ronaldo_scores_a_goal =="yes": 
+    print("siu!")
+elif ronaldo_scores_a_goal =="no":
+    print("oh no!")
+else:
+    print("huh?")
+print("THE END")        
