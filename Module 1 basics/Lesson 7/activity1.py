2e5ebed59ab3be1c5941dc80e2af9a36ccf72761
Sanaathani Sithara <dr.jyothini.mallesh@gmail.com>
2026-07-06 12:40:09 +0530

    m1 l7 a1

---
+++ Module 1 basics/Lesson 7/activity1.py
@@ -0,0 +1,9 @@
+x=1
+y=1
+print("checking equality for x & y",x==y)
+print("checking identity of x & y",x is y)
+
+ur_friend=["Maryam","Shirin"]
+my_friend=["Maryam","Shirin"]
+print("checking equality for ur_friend & my_friend", ur_friend==my_friend)
+print("checking identity of ur_friend & my_friend",ur_friend is my_friend )
