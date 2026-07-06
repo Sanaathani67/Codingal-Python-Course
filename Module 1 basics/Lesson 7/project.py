b0d678fc6d4d283ab453706fbfdc4f052d3d5079
Sanaathani Sithara <dr.jyothini.mallesh@gmail.com>
2026-07-06 13:07:49 +0530

    finished m1 l7 p1

--- Module 1 basics/Lesson 7/project.py
+++ Module 1 basics/Lesson 7/project.py
@@ -1,2 +1,13 @@
-character="A"
-print(ord(character))
+character=input("enter a character : ")
+ascii_value=ord(character)
+print(ascii_value)
+if ascii_value in range(97,123):
+    print("lower case alphabet")
+elif ascii_value in range(65,91):
+    print("upper case alphabet")
+elif ascii_value in range(48,58):
+    print("Digit")
+elif ascii_value in range(32):
+    print("space")
+else:
+    print("special character")  
