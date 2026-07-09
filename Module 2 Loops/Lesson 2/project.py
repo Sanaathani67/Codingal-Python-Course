a3335e3584893f7a6e6598619e52ab066cc4519a
Sanaathani Sithara <dr.jyothini.mallesh@gmail.com>
2026-07-09 15:23:58 +0530

    finished l2 m2 p1

---
+++ Module 2 Loops/Lesson 2/project.py
@@ -0,0 +1,4 @@
+number = int(input("Enter the number: "))
+n_power = int(input("Enter the power: "))
+for i in range(1,n_power + 1):
+    print(f"{number} = {number ** i}")#I related to L8 A2 and L8 project
