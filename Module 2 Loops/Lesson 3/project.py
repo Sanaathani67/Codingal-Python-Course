788b994388910ee41eeba0e85b87dabf2dcdc6ed
Sanaathani Sithara <dr.jyothini.mallesh@gmail.com>
2026-07-13 12:49:42 +0530

    finished m2 l3 p1

---
+++ Module 2 Loops/Lesson 3/project.py
@@ -0,0 +1,8 @@
+num = input("Enter a number: ")
+
+if num.isdigit():#researched on google
+    count = len(num)#researched on google
+    print("Total number of digits:", count)
+else:
+    print("Invalid input! Please enter digits only.")
+
