7e1ee6118a9c1f034e3d95732eacac8255808adb
Sanaathani Sithara <dr.jyothini.mallesh@gmail.com>
2026-07-13 12:58:46 +0530

    altered m2 l3 p1

--- Module 2 Loops/Lesson 3/project.py
+++ Module 2 Loops/Lesson 3/project.py
@@ -1,7 +1,7 @@
-num = input("Enter a number: ")
 
-if num.isdigit():#researched on google
-    count = len(num)#researched on google
+n=(input("enter a number find the number of digits:"))
+if n .isdigit():#researched on google
+    count = len(n)#researched on google
     print("Total number of digits:", count)
 else:
     print("Invalid input! Please enter digits only.")
