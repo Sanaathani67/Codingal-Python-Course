7d97e340f1a20cfbb532263afcd60cedec62cdc1
Sanaathani Sithara <dr.jyothini.mallesh@gmail.com>
2026-07-14 12:26:03 +0530

    finished power calculator p &ex of l4

--- Module 2 Loops/Lesson 2/project.py
+++ Module 2 Loops/Lesson 2/project.py
@@ -1,4 +1,14 @@
 number = int(input("Enter the number: "))
 n_power = int(input("Enter the power: "))
+result=1
 for i in range(1,n_power + 1):
-    print(f"{number} = {number ** i}")#I related to L8 A2 and L8 project
+    result=result*number
+
+print(result)
+   
+   
+   
+   
+   
+   
+# print(f"{number} = {number ** i}")#I related to L8 A2 and L8 project
---
+++ Module 2 Loops/Lesson 4/example2.py
@@ -0,0 +1,4 @@
+section=["cypress","magnolia"]
+for i in section:
+    for j in range(1,4):
+        print(f"section {i}-roll.no{j}")
