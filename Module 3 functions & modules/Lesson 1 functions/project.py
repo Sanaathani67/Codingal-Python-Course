8aa6ba9d4c1c96d523a9981df4511f9e6a796ea0
Sanaathani Sithara <dr.jyothini.mallesh@gmail.com>
2026-07-28 13:01:33 +0530

    finished m3 l1 p1

---
+++ Module 3 functions & modules/Lesson 1 functions/project.py
@@ -0,0 +1,35 @@
+def greet_customer():
+    print("Welcome to the Art Supplies Store!")
+    print("Find everything you need for your next masterpiece!")
+
+greet_customer()
+
+price_per_item = float(input("Enter the price per art item: "))
+items_bought = int(input("How many art items do you want? "))
+
+def calculate_total(price, items):
+    total = price * items
+    return total
+
+my_total_bill = calculate_total(price_per_item, items_bought)
+print("Total Cost:", round(my_total_bill, ))
+
+def thank_you_message(items):
+    if items >= 5:
+        return "Wow, thanks for stocking up on art supplies!"
+    else:
+        return "Thanks for shopping with us!"
+
+closing_message = thank_you_message(items_bought)
+
+print("\n----- Art Supplies Receipt -----")
+print("Price per item: ", round(price_per_item, ))
+print("Items purchased:", items_bought)
+print("Total cost: ", round(my_total_bill, ))
+
+
+def closing_message():
+    print("Thank you for visiting the Art Supplies Store!")
+    print("Come back soon for more creative supplies!")
+
+closing_message()
