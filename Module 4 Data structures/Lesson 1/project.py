f8cadef5eb609fe860d01aab61e2068430852ed8
Sanaathani Sithara <dr.jyothini.mallesh@gmail.com>
2026-08-06 18:59:10 +0530

    finished module 4 lesson1 project 1

--- Module 4 Data structures/Lesson 1/project.py
+++ Module 4 Data structures/Lesson 1/project.py
@@ -35,4 +35,14 @@ print("Largest Mark:", largest)
 
 
 
+fruits = ["apple", "banana", "apple", "guava", "apple", "pineapple"]
 
+search = input("Search for a fruit(only apple banana guava pineapple): ")
+
+count = 0
+
+for fruit in fruits:
+    if fruit == search:
+        count = count + 1
+
+print(search, "comes", count, "times in the list.")
