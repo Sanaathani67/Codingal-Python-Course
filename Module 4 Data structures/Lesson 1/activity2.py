c01429f7f5d81311b1335905c93acef6112769e3
Sanaathani Sithara <dr.jyothini.mallesh@gmail.com>
2026-08-06 18:52:34 +0530

    finished m4 a3 l1

---
+++ Module 4 Data structures/Lesson 1/activity2.py
@@ -0,0 +1,12 @@
+fruits = ["apple", "banana", "apple", "guava", "apple", "pineapple"]
+
+search = input("Search for a fruit: ")
+
+count = 0
+
+for fruit in fruits:
+    if fruit == search:
+        count = count + 1
+
+print(search, "occurs", count, "times in the list.")
+
---
+++ Module 4 Data structures/Lesson 1/project.py
@@ -0,0 +1,38 @@
+marks = [78, 85, 92, 67, 88, 74]
+
+print("marks list:", marks)
+print("Type:", type(marks))
+
+print("number of marks:", len(marks))
+
+print("first mark:", marks[0])
+print("last mark:", marks[-1])
+
+print("first three marks:", marks[0:3])
+print("reversed marks list:", marks[::-1])
+
+print("checking first and last digit of each mark:")
+for mark in marks:
+    mark_str = str(mark)
+    print("mark:", mark, "first digit:", mark_str[0], "last digit:", mark_str[-1])
+
+total = 0
+for mark in marks:
+    total += mark
+
+average = total / len(marks)
+
+sorted_marks = sorted(marks)
+smallest = sorted_marks[0]
+largest = sorted_marks[-1]
+
+print("Summary")
+print("Total Marks:", total)
+print("Average Marks:", average)
+print("Smallest Mark:", smallest)
+print("Largest Mark:", largest)
+
+
+
+
+
